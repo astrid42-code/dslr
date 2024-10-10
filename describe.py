89826e95b676c16ab75f8d6acc59f2c5f6e20650
Astrid <pingu@air-de-astrid.home>
2024-10-10 11:49:34 +0200

    data load modified

--- describe.py
+++ describe.py
@@ -1,22 +1,27 @@
 from pandas import read_csv, DataFrame
 import matplotlib.pyplot as plt
-import os
 import numpy as np
+import sys
 
 
 def main():
     # Load data
+
+    if len(sys.argv) != 2:
+        print("You need to give a path as argument")
+        exit()
     try:
-        data_path = os.path.join(os.getcwd(), "datasets/dataset_train.csv")
+        data_path = "datasets/" + sys.argv[1]
+        # print(data_path)
         dataset = read_csv(data_path)
     except Exception:
         return print("ERROR : Unvalid path/file for data")
-    # print(dataset)
+    # print(dataset)  # type = <class 'pandas.core.frame.DataFrame'>
 
     data = read_csv("datasets/dataset_train.csv").to_numpy().transpose()
-    print(data)
+    # print(data) # type = <class 'numpy.ndarray'>
 
-    
+
 
 if __name__ == "__main__":
     main()
